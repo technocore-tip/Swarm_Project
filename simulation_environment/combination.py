dddeffc4c7a919b260356d2abd2fccbd4316f319
paul028 <paulvincentnonat@gmail.com>
2020-01-05 15:06:39 +0800

    updates

---
+++ simulation_environment/combination.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 10:34:38 2020
+
+@author: Paul Vincent Nonat
+"""
+from environment import draw_windows,draw_swarm,distance_magnitude,relative_distance,update_pairwisedistance,position_vector
+import time
+import threading
+import math
+from random import randint
+
+import numpy as np
+from itertools import combinations
+   
+import scipy.stats
+
+
+
+def mt_shuffle():
+    np.random.shuffle(pairwise_list)
+N=70
+rho_bar=10
+mu=5
+l=5*rho_bar
+times=pow(2,-8)
+
+sigma_p=0.25
+particles=list()
+
+min_val = 0.1
+max_val = 25
+
+
+
+# Plot distribution PDF
+dist = scipy.stats.truncnorm((min_val - rho_bar) / sigma_p, (max_val - rho_bar) / sigma_p, loc=rho_bar, scale=sigma_p)
+
+rho_k = dist.rvs(N)
+
+win = draw_windows(1024,1024) #draw window with width = 700 and height = 600
+robots = draw_swarm(N,win,l) #draw 7 swarm in win
+win.getMouse() #blocking call
+
+for i in range(1,N+1,1):
+    particles.append([i,rho_k[i-1]])
+
+
+pairwise_list= list(combinations(particles,2))
+mt_shuffle()
+
+
+while(1):
+
+    for z in range(len(pairwise_list)):
+        robot_j = robots[pairwise_list[z][0][0]-1]
+        rho_j= pairwise_list[z][0][1]
+        robot_k= robots[pairwise_list[z][1][0]-1]
+        rho_k = pairwise_list[z][1][1]
+        print(robots[pairwise_list[z][0][0]-1])
+        print(robots[pairwise_list[z][1][0]-1])
+        xj,yj,xk,yk=update_pairwisedistance(robot_j,rho_j,robot_k,rho_k,times,mu,win)
+    
+        #time.sleep(10)
+        #robots[pairwise_list[z][0][0]-1].move(xj + win.getWidth()/2,(win.getHeight()/2) - yj) # move bot to new position
+        #robots[pairwise_list[z][1][0]-1].move(xk + win.getWidth()/2,(win.getHeight()/2) - yk)
+        robots[pairwise_list[z][0][0]-1].move(xj,-yj) # move bot to new position
+        robots[pairwise_list[z][1][0]-1].move(xk,-yk)
+        print(robots[pairwise_list[z][0][0]-1])
+        print(robots[pairwise_list[z][1][0]-1])
+        print("Interaction")
+        print(z)
+        print(pairwise_list[z])        
+        #win.getMouse() 
+   # win.getMouse() 
+    mt_shuffle()
---
+++ simulation_environment/environment.py
@@ -0,0 +1,111 @@
+from graphics import * #using library from mcsp.wartburg.edu/zelle/python/graphics.py
+import time
+import threading
+import math
+from random import randint
+import numpy as np
+
+lock = threading.Lock()
+
+def draw_windows(w,h): #function for creating the simulation space
+	#create windows
+	#print('create windows with name : swarm, width : ',w,', height : ',h)
+	return GraphWin('swarm',w,h)
+	pass
+
+def draw_swarm(n,win,l): #function for drawing robot nodes
+	robot =[]
+	for x in range(0,n):
+		#robot.append(Circle(Point( randint(20,win.getWidth-20) , randint(20,win.getHeight()-20) ), 0.5))
+		robot.append(Circle(Point( randint(-(win.getWidth()/4),win.getWidth()/4)+win.getWidth()/2, (win.getHeight()/2)-randint(-(win.getHeight()/4),win.getHeight()/4) ), 2))
+		#print(robot)
+		#print('create robot with number ',x,' point x : ',x*200+40,', y : 200 , r : 5')
+		robot[x].setFill('blue')
+		robot[x].draw(win)
+		pass
+	#print(robot)
+	return robot
+	pass
+
+def distance_vector(P1,P2,win):#function to calculate distance between two points
+    x1= P1.getX() - win.getWidth()/2 #win.getWidth()2 translate the given points to cartseian plane with origin at center of the plane instead at the side of window
+    x2 = P2.getX() - win.getWidth()/2
+    
+    y1=win.getHeight()/2 - P1.getY()
+    y2=win.getHeight()/2 - P2.getY()
+    
+    x = x1 - x2 #P1.getX()-P2.getX()
+    y = y1 - y2 #P1.getY()-P2.getY()
+    return x,y
+    pass
+
+def position_vector(P,win):
+    x = P.getX() - win.getWidth()/2
+    y = win.getHeight()/2 - P.getY()
+    return x,y
+
+def distance_magnitude(x,y):
+	#calculate magnitude of distance vector
+	return math.sqrt(x*x+y*y)
+	pass
+
+def relative_distance(robot,robots,win):#calculate relative distance of a robot to all other robots
+	x_total=0
+	y_total=0
+	for r in robots:
+		x,y = distance_vector(robot.getCenter(),r.getCenter())
+		x_total+=x
+		y_total+=y
+		pass
+	return x_total,y_total
+
+def update_pairwisedistance(robot_j,rho_j,robot_k,rho_k,times,mu,win):
+    x_p,y_p = distance_vector(robot_j.getCenter(),robot_k.getCenter(),win) #xj-xk , yj-yk
+    pdist = distance_magnitude(x_p,y_p) # |rj - rk|
+    print("distance_vector")
+    print(x_p)
+    print(y_p)
+    print("Attraction Repulsion")
+    print("Theta")
+    theta= np.arctan(y_p/x_p)
+    print("J particle movement")
+    xrj = mu*np.cos(theta)*math.tanh(pdist-rho_j)*times
+    yrj = mu*np.sin(theta)*math.tanh(pdist-rho_j)*times
+    print("K particle movement")   
+    xrk = mu*np.cos(theta)*math.tanh(pdist-rho_k)*times
+    yrk = mu*np.sin(theta)*math.tanh(pdist-rho_k)*times
+    print("J Previous Pos")
+    xj,yj= position_vector(robot_j.getCenter(),win)  #update position vectors
+    print(xj)
+    print(yj)
+    print("J new Pos")
+    xj= xj + xrj
+    yj = yj + yrj
+    print(xj)
+    print(yj)
+    
+    print("K Previous Pos")
+    xk,yk= position_vector(robot_k.getCenter(),win)  #update position vectors
+    print(xk)
+    print(yk)
+    xk= xk + xrk
+    yk = yk + yrk
+    print("K new Pos")
+    print(xk)
+    print(yk)
+    return xrj,yrj,xrk,yrk
+	#calculate the pairwise distance
+
+
+
+#def main():
+#	#main program
+#	win = draw_windows(1920,1080) #draw window with width = 700 and height = 600
+#	robots = draw_swarm(1000,win) #draw 7 swarm in win
+#	win.getMouse() #blocking call
+#	simulate_meeting_point(robots) #start simulation meeting point
+#	win.getMouse()
+#	pass
+#
+#if __name__ == '__main__':
+#	main()
