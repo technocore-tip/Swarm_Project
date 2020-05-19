c9442bc5c7d6729f8e69d6b75ca696b40922a6b7
paul028 <paulvincentnonat@gmail.com>
2020-05-19 14:08:41 +0800

    RSSI Experiment

--- ZIGBEE_LOCALIZATION/localize.py
+++ ZIGBEE_LOCALIZATION/localize.py
@@ -9,8 +9,16 @@ import numpy as np
 
 headers=['D1','D2','D3','D4','x','y']
 df= pd.read_csv('rss_experiment.csv',names=headers)
-#                         x1,y1 x2 y2  x3 y3   x4 y 
+
+actual_x=df['x'].values
+actual_x=np.delete(actual_x,0)
+actual_x=actual_x.astype('float64')
+actual_y=df['y'].values
+actual_y=np.delete(actual_y,0)
+actual_y=actual_y.astype('float64')
+
 result=list()
+error=list()
 for n in range(1,df.shape[0],1):
     d=df.iloc[n].values.astype('float64')
     ref_node=np.array([[2,2],[-2,2],[-2,-2],[2,-2]])
@@ -23,5 +31,7 @@ for n in range(1,df.shape[0],1):
     [(pow(ref_node[2][0],2)-pow(ref_node[3][0],2))+(pow(ref_node[2][1],2)-pow(ref_node[3][1],2)-pow(np.sqrt(1/d[2]),2)+pow(np.sqrt(1/d[3]),2))]])
     xy=a/X
     x,y=xy[2][0],xy[0][1]
-    result.append([x,y])
-    print("Trial=",n,"x=",x,"y=",y)
+    rmse=np.sqrt(pow(actual_x[n-1]-x,2)+pow(actual_y[n-1]-y,2))
+    result.append([x,y,rmse])
+    error.append(rmse)
+    print("Trial=",n,"x=",x,"y=",y,"RMSE=",rmse)
